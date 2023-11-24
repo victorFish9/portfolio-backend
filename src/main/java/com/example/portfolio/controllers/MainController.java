f39bd28d4495e1d544662686e2c5a1b6fd5c1fc7
Victor <aue228lublupacanov@gmail.com>
2023-11-24 19:40:31 +0200

    ?

--- src/main/java/com/example/portfolio/controllers/MainController.java
+++ src/main/java/com/example/portfolio/controllers/MainController.java
@@ -1,2 +1,13 @@
-package com.example.portfolio.controllers;public class MainController {
+package com.example.portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/home")
+    public String home(Model model){
+        return "home";
+    }
 }
