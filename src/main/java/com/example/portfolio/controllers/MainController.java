5fa9c5ddc95aebd4e1e43e3d66b83d530e3b52a2
Victor <aue228lublupacanov@gmail.com>
2023-11-24 19:53:48 +0200

    endpotin in home changed

--- src/main/java/com/example/portfolio/controllers/MainController.java
+++ src/main/java/com/example/portfolio/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(Model model){
         return "home";
     }
